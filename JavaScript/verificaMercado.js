afe47de92b5b0fc673cdd7aa91734486ec4fd057
CarlosDv93 <carlos9_cv@hotmail.com>
2016-04-01 00:01:15 -0300

    teste do Mercado
    testando o envio

---
+++ JavaScript/verificaMercado.js
@@ -0,0 +1,12 @@
+
+function verifica() {
+    var nome = document.getElementsById("formMercado").elements.namedItem("nome").value;
+    var nome1 = document.getElementById("form1").elements.namedItem("nome1").value;
+    var tipo = document.getElementsById("tipo");
+    var qtde = document.getElementsById("qtde");
+    var negocio = document.getElementsById("negocio");
+    var codigo = document.getElementsById("formMercado").elements.namedItem("codigo").value;
+    // verificaNome(nome);
+    console.log(nome);
+};
+
